a1a01a7d4948330df520a20545c2707ee0c34153
vaibhaveet <91821735+vaibhaveet@users.noreply.github.com>
2022-11-03 16:37:25 +0530

    Update Python60.py

--- Python60.py
+++ Python60.py
@@ -1,15 +1,10 @@
-from math import sqrt
-n = 1
-prime_flag = 0
-
-if(n > 1):
-	for i in range(2, int(sqrt(n)) + 1):
-		if (n % i == 0):
-			prime_flag = 1
-			break
-	if (prime_flag == 0):
-		print("True")
-	else:
-		print("False")
+num = 11
+if num > 1:
+    for i in range(2, int(num/2)+1):
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
 else:
-	print("False")
+    print(num, "is not a prime number")
