29ae7076784f97304615e2a86ba12c9d0f2892ab
vaibhaveet <91821735+vaibhaveet@users.noreply.github.com>
2022-11-03 15:48:22 +0530

    Update Python60.py

--- Python60.py
+++ Python60.py
@@ -1,12 +1,11 @@
 
 num = 17 
-    for ( i=2; i < num; i++):
-        if (num %  i==0):
-            print(num, "is not a prime number")
-            break
+for ( i=2; i < num; i++):
+    if (num %  i==0):
+        print(num, "is not a prime number")
+        break
     else
         print (num, "is a prime number")
-        
 else
     print (num, "is not a prime number")
     
